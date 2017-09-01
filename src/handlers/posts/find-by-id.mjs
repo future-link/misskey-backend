bf3d400afaf179874914f803da671ba48725b962
otofune <otofune@gmail.com>
2017-09-01 11:46:24 +0900

    db | モデルは大文字で始まるように
    もともとのに合わせる
    で、なんか知らないけどひとつだけ大文字だったのが統一された

--- src/handlers/posts/find-by-id.mjs
+++ src/handlers/posts/find-by-id.mjs
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
-import { post } from '../../db'
+import { Post } from '../../db'
 
 export default async (ctx, id) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     ctx.throw(404, 'there are no posts has given ID.')
-  const content = await post.findById(id)
-  if (!content)
+  const post = await Post.findById(id)
+  if (!post)
     ctx.throw(404, 'there are no posts has given ID.')
-  ctx.body = content.toObject()
+  ctx.body = post.toObject()
 }
