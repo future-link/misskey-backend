bf0f9b483b8874ef8e56b0dbc0fb347e048578b9
otofune <otofune@gmail.com>
2017-09-02 16:49:18 +0900

    transformUser | 使われてない情報を徹底的に削除

--- src/transformers/user.mjs
+++ src/transformers/user.mjs
@@ -1,10 +1,21 @@
 export default (user) => {
   const clone = Object.assign({}, user)
-  // 定義から抹消したい情報
+  // 利用されていない・不要な情報
+  delete clone.birthday
+  delete clone.color
+  delete clone.credit
+  delete clone.pinnedPost
   delete clone.avatarPath
   delete clone.bannerPath
   delete clone.wallpaperPath
-  // ユーザーにとって必要のない情報
+  delete clone.isDeleted
+  delete clone.isPrivate
+  delete clone.isPro
+  delete clone.isStaff
+  delete clone.isSuspended
+  delete clone.isVerified
+  // パブリックに開示する必要のない情報
+  delete clone.timelineReadCursor
   delete clone.screenNameLower
   // 機密にすべき情報
   delete clone.email
