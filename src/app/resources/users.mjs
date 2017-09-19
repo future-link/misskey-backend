e0c95c654eb3cc276165c36cd59e349673661c15
otofune <otofune@gmail.com>
2017-09-20 06:42:08 +0900

    /users 実装
    utilsに関数生えまくってるけど大丈夫か？

--- src/app/resources/users.mjs
+++ src/app/resources/users.mjs
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 import app from '../app'
 import { User } from '../../db'
 
-import { denyNonAuthorized } from '../utils'
+import { denyNonAuthorized, getPropWithDefaultAndVerify as get } from '../utils'
 
 const getUserById = async id => {
   let user = null
@@ -18,6 +18,27 @@ const getUserById = async id => {
   }
   return user
 }
+
+app.use(route.get('/users', async (ctx) => {
+  const limit = Number.parseInt(await get(ctx.query, 'limit', 100, v => {
+    const n = Number.parseInt(v)
+    if (!n) return false
+    // must not over 200
+    if (n > 200) return false
+    // must not negative
+    if (n <= 0) return false
+    return true
+  }, `query 'limit'`))
+  const skip = Number.parseInt(await get(ctx.query, 'skip', 0, v => {
+    const n = Number.parseInt(v)
+    if (!n) return false
+    // must not negative
+    if (n <= 0) return false
+    return true
+  }, `query 'skip'`))
+  const users = await User.find().skip(skip).limit(limit)
+  ctx.body = { users: users.map(user => user.toObject()) }
+}))
 
 app.use(route.get('/users/:id', async (ctx, id) => {
   const user = await getUserById(id)
--- src/app/utils.mjs
+++ src/app/utils.mjs
@@ -1,3 +1,17 @@
 export async function denyNonAuthorized (ctx) {
   if (!ctx.state.user) ctx.throw(401, 'must authenticate to request this endpoint.')
 }
+
+export async function getPropWithDefaultAndVerify (object, key, preset, verifier = null, pne = null) {
+  if (!object[key]) return preset
+  if (!verifier) return object[key]
+  const vr = verifier(object[key])
+  if (vr.constructor.name !== 'Promise' && vr) return object[key]
+  if (await vr) return object[key]
+  // verify error (pne: prop name in error)
+  const e = new Error(`${pne || key} is invalid.`)
+  // mimic ctx.throw's error
+  e.expose = true
+  e.status = 400
+  throw e
+}
