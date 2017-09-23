496dfae28e377fd330350380a031f8462eab84e7
otofune <otofune@gmail.com>
2017-09-23 13:34:52 +0900

    DELETE /account/posts/:id, PUT /account/stars/:id, DELETE /account/stars/:id の実装

--- src/app/resources/posts.mjs
+++ src/app/resources/posts.mjs
@@ -4,7 +4,8 @@ import mongoose from 'mongoose'
 import app from '../app'
 import { Post, PostLike } from '../../db'
 
-import { getLimitAndSkip } from '../utils'
+import { getLimitAndSkip, denyNonAuthorized } from '../utils'
+import redis from '../../redis'
 
 app.use(route.get('/posts', async (ctx) => {
   const [limit, skip] = await getLimitAndSkip(ctx)
@@ -20,7 +21,7 @@ app.use(route.get('/posts/:id', async (ctx, id) => {
 }))
 
 app.use(route.get('/posts/:id/stargazers', async (ctx, id) => {
-  if (!mongoose.Types.ObjectId.isValid(id)) ctx.throw(404, 'there are no stargazers to the post has given ID.')
+  if (!mongoose.Types.ObjectId.isValid(id)) ctx.throw(404, 'there are no posts has given ID.')
   const [limit, skip] = await getLimitAndSkip(ctx)
   const likes = await PostLike.find({
     post: id
@@ -31,4 +32,40 @@ app.use(route.get('/posts/:id/stargazers', async (ctx, id) => {
   const stargazers = []
   likes.forEach(like => { stargazers.push(like.user.toObject()) })
   ctx.body = { stargazers }
+}))
+
+app.use(route.delete('/account/posts/:id', async (ctx, id) => {
+  await denyNonAuthorized(ctx)
+  if (!mongoose.Types.ObjectId.isValid(id)) ctx.throw(404, 'there are no posts has given ID.')
+  const post = await Post.findById(id)
+  if (!post || post.user !== ctx.state.account.id) ctx.throw(404, 'there are no posts has given ID.')
+  await post.remove()
+  ctx.status = 204
+}))
+
+app.use(route.put('/account/stars/:id', async (ctx, id) => {
+  await denyNonAuthorized(ctx)
+  if (!mongoose.Types.ObjectId.isValid(id)) ctx.throw(404, 'there are no posts has given ID.')
+  const post = await Post.findById(id)
+  if (!post) ctx.throw(404, 'there are no posts has given ID.')
+  const content = {
+    post: post.id,
+    user: ctx.state.account.id }
+  if (await PostLike.findOne(content)) ctx.throw(409, 'already starred.')
+  const star = new PostLike(content)
+  await star.save()
+  ctx.status = 204
+}))
+
+app.use(route.delete('/account/stars/:id', async (ctx, id) => {
+  await denyNonAuthorized(ctx)
+  if (!mongoose.Types.ObjectId.isValid(id)) ctx.throw(404, 'there are no posts has given ID.')
+  const post = await Post.findById(id)
+  if (!post) ctx.throw(404, 'there are no posts has given ID.')
+  const star = await PostLike.findOne({
+    post: post.id,
+    user: ctx.state.account.id })
+  if (!star) ctx.throw(404, 'there are no stars to the post has given ID.')
+  await star.remove()
+  ctx.status = 204
 }))
